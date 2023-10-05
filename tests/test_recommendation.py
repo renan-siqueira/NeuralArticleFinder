83fe3fe9a3287d0f0851e81643b17a343b733feb
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-05 12:46:03 -0300

    feat: test_recommendation.py adapted to pylint rules

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,3 +1,6 @@
+"""
+Unit tests for the recommendation functionalities of NeuralArticleFinder project.
+"""
 import unittest
 import numpy as np
 from gensim.models import Word2Vec
@@ -5,6 +8,7 @@ from src.recommendation.recommend import vectorize_document, most_similar_docs
 
 
 class TestRecommendationFunctions(unittest.TestCase):
+    """Test suite for recommendation functions."""
 
     @classmethod
     def setUpClass(cls):
@@ -19,6 +23,8 @@ class TestRecommendationFunctions(unittest.TestCase):
         )
 
     def test_vectorize_document(self):
+        """Tests vector representation of documents using Word2Vec model."""
+
         # Testing a document with words known by the model
         vec = vectorize_document(['hello', 'world'], self.model)
         self.assertTrue(np.any(vec))
@@ -28,6 +34,8 @@ class TestRecommendationFunctions(unittest.TestCase):
         self.assertTrue(np.all(vec == 0))
 
     def test_most_similar_docs(self):
+        """Tests ranking of documents based on their similarity to a target document."""
+
         # Very similar document
         top_docs = most_similar_docs(['hello', 'world'], self.model, self.docs)
         self.assertEqual(top_docs[0][0], ['hello', 'world'])
