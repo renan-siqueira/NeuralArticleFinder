24733ed479c9beae9abfdf91f08ba18b61ce5dc1
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-05 12:43:22 -0300

    feat: test_preprocessing.py adapted to pylint rules

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,10 +1,15 @@
+"""
+Unit tests for the preprocessing functionalities of NeuralArticleFinder project.
+"""
 import unittest
 from src.preprocessing.preprocess import preprocess
 
 
 class TestPreprocessFunction(unittest.TestCase):
+    """Test suite for the preprocess function."""
 
     def test_preprocess(self):
+        """Tests the basic functionality of the preprocess function."""
         input_text = "This is a sample text with some stopwords and other relevant words."
         expected_output = ['sample', 'text', 'stopwords', 'relevant', 'word']
 
